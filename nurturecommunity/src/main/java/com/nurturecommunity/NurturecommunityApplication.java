6c0f1de331f67fd414527cc33909c26fd803f7ff
arshdeepk61 <48295231+arshdeepk61@users.noreply.github.com>
2021-06-08 18:20:28 +0530

    Add files via upload

---
+++ nurturecommunity/src/main/java/com/nurturecommunity/NurturecommunityApplication.java
@@ -0,0 +1,13 @@
+package com.nurturecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurturecommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurturecommunityApplication.class, args);
+	}
+
+}
