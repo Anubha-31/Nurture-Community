4b9ebae01785075c3989db301ace370e1c9025d3
Roxy Palma <leo_9319@yahoo.com>
2021-06-16 02:13:09 +0600

    signup link and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from "@/views/SignUp";
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/sign-up',
+    name: 'Sign Up',
+    component: SignUp
   },
 ]
 
