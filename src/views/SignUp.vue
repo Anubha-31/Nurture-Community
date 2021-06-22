c20290ea95f92163c786e1fb648597f9c0beebab
Roxy Palma <leo_9319@yahoo.com>
2021-06-22 21:49:28 +0600

    responsive signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,6 +5,10 @@
         <router-link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <img src="..\assets\images\logo.png" alt="" class="w-36">
         </router-link>
+        <nav class=" md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+          <a class="mr-5 hover:text-gray-900 cursor-pointer">About Us</a>
+          <a class="mr-5 hover:text-gray-900 cursor-pointer">Contact Us</a>
+        </nav>
       </div>
     </header>
     <div class="container mx-auto px-6">
