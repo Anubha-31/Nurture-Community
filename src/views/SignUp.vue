52047b25a134e3e646d4defaaec92902738f2525
Roxy Palma <leo_9319@yahoo.com>
2021-06-29 18:55:12 +0600

    test data

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -18,14 +18,14 @@
       </div>
 
       <div class="mt-5 md:mt-10 md:w-1/2 md:mx-auto">
-        <form action="#" method="POST">
+        <form method="POST" @submit.prevent="formSubmit">
           <div class="shadow overflow-hidden rounded-lg border border-gray-200">
             <div class="px-4 py-5 bg-white sm:p-6">
               <div class="grid grid-cols-6 gap-6">
 
                 <div class="col-span-6">
                   <label for="user_type" class="block text-sm font-medium text-gray-700">Sign Up As</label>
-                  <select id="user_type" name="user_type" autocomplete="user_type" v-model="user_type" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <select id="user_type" name="user_type" autocomplete="user_type" v-model="user.user_type" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     <option value="restaurant">Restaurant Representative</option>
                     <option value="client">Client</option>
                   </select>
@@ -46,12 +46,12 @@
                   <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md">
                 </div>
 
-                <div class="col-span-6 sm:col-span-3 lg:col-span-3" v-if="user_type === 'restaurant'">
+                <div class="col-span-6 sm:col-span-3 lg:col-span-3" v-if="user.user_type === 'restaurant'">
                   <label for="restaurant_name" class="block text-sm font-medium text-gray-700">Restaurant's Name</label>
                   <input type="text" name="restaurant_name" id="restaurant_name" autocomplete="restaurant-name" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md">
                 </div>
 
-                <div class="col-span-6 sm:col-span-3 lg:col-span-3" v-if="user_type === 'restaurant'">
+                <div class="col-span-6 sm:col-span-3 lg:col-span-3" v-if="user.user_type === 'restaurant'">
                   <label for="license_number" class="block text-sm font-medium text-gray-700">License Number</label>
                   <input type="text" name="license_number" id="license_number" autocomplete="license-number" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md">
                 </div>
@@ -77,7 +77,7 @@
             </div>
             <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
               <button type="submit" class="inline-flex w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Save
+                Sign Up
               </button>
             </div>
           </div>
@@ -97,28 +97,29 @@ export default {
   components: {Footer},
   data() {
     return {
-      user_type: "restaurant",
-      first_name: "",
-      last_name: "",
-      email: "",
-      restaurant_name: "",
-      license_number: "",
-      phone: "",
-      address: "",
-      zip: ""
+      user: {
+        user_type: "restaurant",
+        first_name: "",
+        last_name: "",
+        email: "",
+        restaurant_name: "",
+        license_number: "",
+        phone: "",
+        address: "",
+        zip: ""
+      }
     }
   },
-  mounted() {
-    // axios.post('https://jsonplaceholder.typicode.com/posts', {
-    //   title: 'foo',
-    //   body: 'bar',
-    //   userId: 1,
-    // })
-    // .then((response) => {
-    //   console.log(response);
-    // }, (error) => {
-    //   console.log(error);
-    // });
+  methods: {
+    formSubmit: function () {
+      alert('foobar');
+      // axios.post('https://jsonplaceholder.typicode.com/posts', this.user)
+      // .then((response) => {
+      //   console.log(response);
+      // }, (error) => {
+      //   console.log(error);
+      // });
+    }
   }
 }
 </script>
