ec3eb0f9de5d28732fc9f4e1da7a5d6846a8b44c
Roxy Palma <leo_9319@yahoo.com>
2021-07-11 00:25:45 +0600

    redesign signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -125,7 +125,7 @@
                   <input name="country" id="country" cols="30" rows="4" v-model="user.country" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"/>
                 </div>
 
-                <div class="col-span-6">
+                <div class="col-span-6 sm:col-span-3">
                   <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP / Postal*</label>
                   <input type="text" name="postal_code" id="postal_code" v-model="user.zip" autocomplete="postal-code" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md" required>
                 </div>
@@ -136,7 +136,7 @@
                 </div>
               </div>
             </div>
-            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <div class="px-4 py-3 bg-gray-50 text-right sm:pt-6 sm:pb-8">
               <button type="submit" class="inline-flex w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Sign Up
               </button>
