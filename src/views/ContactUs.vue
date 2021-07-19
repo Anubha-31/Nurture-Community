4f59afca144c9601695ef2ba2f4f20fc347ae7ec
Anubha <anubhasharma2021@outlook.com>
2021-07-20 02:21:29 +0530

    responsive flex for contact us

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -6,7 +6,7 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
       <div class="mt-1 overflow-hidden">
         <div class="grid grid-cols-1 md:grid-cols-2">
-          <div class="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
+          <div class="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg order-last  md:order-first ">
             <h1
               class="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight"
             >
