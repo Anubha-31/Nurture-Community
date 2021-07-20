299011cd879599e548c442a30e1eb4a88b426d12
Roxy Palma <leo_9319@yahoo.com>
2021-07-21 05:07:04 +0600

    better delete modal

--- src/components/DeleteFoodItemModal.vue
+++ src/components/DeleteFoodItemModal.vue
@@ -1,39 +1,35 @@
 <template>
   <transition name="modal-fade">
+    <form action="#" method="POST" @submit.prevent="deleteFoodQuantity">
     <div class="modal-backdrop" role="dialog">
-      <div class="modal relative rounded-md" ref="modal">
-        <header class="modal-header">
-          <slot name="header">
-
-            <button type="button" class="font-extrabold px-4 py-1 absolute top-0 right-0" @click="close" aria-label="Close modal">x</button>
-          </slot>
-        </header>
-
-        <form action="#" method="POST" @submit.prevent="deleteFoodQuantity">
-          <section class="modal-body">
-            <slot name="body">
-              <h2 class="pt-5">Are you sure you want to delete this item?</h2>
-              <input type="hidden" v-model="item.id">
-            </slot>
-          </section>
-
-          <footer class="modal-footer">
-            <slot name="footer">
-              <div class="space-x-2">
-                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-800 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  Yes
-                </button>
-                <button type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-700 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" @click="close" aria-label="Close modal">
-                  No
-                </button>
-              </div>
-            </slot>
-          </footer>
-        </form>
+      <div class="bg-white rounded-lg w-4/5 md:w-1/4">
+        <div class="flex flex-col items-start p-4">
+          <div class="flex items-center w-full">
+            <div class="text-gray-900 font-medium text-lg">
+              <img src="../assets/svg/warning.svg" alt="" class="w-6 inline-block">
+              Warning!
+            </div>
+            <svg class="ml-auto fill-current text-gray-700 w-6 h-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" @click="close">
+              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
+            </svg>
+          </div>
+          <hr>
+          <div class="mt-5">Are you sure you want to delete this item?</div>
+          <hr>
+          <input type="hidden" v-model="item.id">
+          <div class="ml-auto mt-5 space-x-2">
+            <button type="submit" class="bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Yes
+            </button>
+            <button type="button" @click="close" class="bg-transparent hover:bg-green-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              No
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+    </form>
   </transition>
-
 </template>
 
 <script>
