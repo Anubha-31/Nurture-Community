19b2edaf2ac070ebd1930fc6a7c6114840bf66c3
Leonard <leo_9319@yahoo.com>
2021-07-22 03:45:07 +0600

    implement QR code generator

--- src/views/QRTest.vue
+++ src/views/QRTest.vue
@@ -1,13 +1,23 @@
 <template>
+  <button @click="qrgen">Generate QR Code</button>
+  <qrcode-vue :value="value" :size="size" level="H" />
 <!--  <qrcode-stream @decode="onDecode"></qrcode-stream>-->
 </template>
 
 <script>
 import { QrcodeStream } from 'vue3-qrcode-reader'
+import QrcodeVue from 'qrcode.vue'
 export default {
   name: "QRTest",
+  data() {
+    return {
+      value: 'https://example.com',
+      size: 300,
+    }
+  },
   components: {
-    QrcodeStream
+    QrcodeStream,
+    QrcodeVue
   },
   methods: {
     onDecode: function (result) {
