210c3c96270654cd606089db85800adda1d26a28
Roxy Palma <leo_9319@yahoo.com>
2021-07-27 19:37:59 +0600

    add claim item button

--- src/views/RestaurantProfile.vue
+++ src/views/RestaurantProfile.vue
@@ -103,7 +103,7 @@
                         <!-- <button class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"> -->
                             <!-- <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg> -->
                         <!-- </button> -->
-                     <button  class="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-4 focus:outline-none hover:bg-yellow-600 rounded-full">+</button>
+<!--                     <button  class="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-4 focus:outline-none hover:bg-yellow-600 rounded-full">+</button>-->
 
                     </div>
                     <div class="px-5 py-3">
@@ -111,6 +111,7 @@
                         <!-- <h2 class="text-gray-700 uppercase">{{ content.item_name }}</h2> -->
                         <!-- <span class="text-gray-400 mt-2">Quantity {{ content.number_of_packets}}</span><br> -->
                         <span class="text-gray-400 mt-2">Quantity: {{ content.numberofPackets}}</span>
+                        <button class="block w-full mt-3 text-white bg-yellow-500 py-2 px-4 focus:outline-none hover:bg-yellow-600 rounded-md">Claim Item</button>
                     </div>
 
                 </div>
