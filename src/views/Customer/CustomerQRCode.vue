a205443545573aae1573283f83063d219fe170d7
Roxy Palma <leo_9319@yahoo.com>
2021-07-30 16:57:17 +0600

    design qr code page

--- src/views/Customer/CustomerQRCode.vue
+++ src/views/Customer/CustomerQRCode.vue
@@ -1,10 +1,32 @@
 <template>
-  <h1>QR Code page</h1>
+  <Header user_type="customer"></Header>
+    <div class="flex justify-center items-center md:min-h-screen">
+      <div class="pt-10 pb-48 md:-mt-56">
+        <qrcode-vue :value="value" :size="size" level="H" />
+      </div>
+    </div>
+  <Footer/>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { QrcodeStream } from 'vue3-qrcode-reader'
+import QrcodeVue from 'qrcode.vue'
 export default {
-  name: "CustomerQRCode"
+  name: "CustomerQRCode",
+  data() {
+    return {
+      value: '{"id":"1", "foodID":2, "emailaddress":"rr@yahoo.com"}',
+      size: 300,
+    }
+  },
+  components: {
+    Header,
+    Footer,
+    QrcodeStream,
+    QrcodeVue
+  }
 }
 </script>
 
