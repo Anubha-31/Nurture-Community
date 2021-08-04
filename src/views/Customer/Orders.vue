326cb33b61b909023feef9842922f187464793c3
Roxy Palma <leo_9319@yahoo.com>
2021-08-04 15:44:59 +0600

    order page design update

--- src/views/Customer/Orders.vue
+++ src/views/Customer/Orders.vue
@@ -26,22 +26,18 @@
                       <div class="flex items-center" data-v-2a66f80c="">
                         <div class="flex flex-col" data-v-2a66f80c="">
                           <div class="w-full flex-none text-lg text-gray-200 font-bold leading-none" data-v-2a66f80c="">{{ item.item_name }}</div>
-                          <div class="flex-auto text-gray-400 my-1" data-v-2a66f80c=""><span class="mr-3 " data-v-2a66f80c="">UI/UX Designer</span><span class="mr-3 border-r border-gray-600  max-h-0" data-v-2a66f80c=""></span><span data-v-2a66f80c="">Cochin, IND</span></div>
+                          <div class="flex-auto text-gray-400 my-1" data-v-2a66f80c=""><span class="mr-3 " data-v-2a66f80c="">{{ item.item_description }}</span><span class="mr-3 border-r border-gray-600  max-h-0" data-v-2a66f80c=""></span><span data-v-2a66f80c="">{{ item.restaurant_name }}</span></div>
                         </div>
                       </div>
                     </div>
                     <div class="flex pt-2  text-sm text-gray-400" data-v-2a66f80c="">
                       <div class="flex-1 inline-flex items-center" data-v-2a66f80c="">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor" data-v-2a66f80c="">
-                          <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" data-v-2a66f80c=""></path>
-                        </svg>
-                        <p class="" data-v-2a66f80c="">1.2k Followers</p>
+                        <i class="far fa-clock"></i>
+                        <p class="pl-1" data-v-2a66f80c="">Pick-up time: {{ item.pickup_time }}</p>
                       </div>
                       <div class="flex-1 inline-flex items-center" data-v-2a66f80c="">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor" data-v-2a66f80c="">
-                          <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd" data-v-2a66f80c=""></path>
-                        </svg>
-                        <p class="" data-v-2a66f80c="">14 Components</p>
+                        <i class="far fa-clock"></i>
+                        <p class="pl-1" data-v-2a66f80c="">Ordered at: {{ item.order_at }}</p>
                       </div>
                       <router-link to="/customer/qr-code" class="title-font font-medium items-center text-gray-900 md:mb-0">
                         <button class="hidden md:flex md:flex-no-shrink bg-yellow-500 hover:bg-yellow-400 px-5 ml-4 py-2 text-xs shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-yellow-500 hover:border-yellow-400 text-white rounded-full transition ease-in duration-300 mr-4">View QR Code</button>
@@ -107,74 +103,98 @@ export default {
       foodItems:[
         {
           item_name: 'Laddu',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Chicken Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
         {
           item_name: 'Beef Fry',
+          item_description: 'lorem ipsum',
           restaurant_name: 'KFC',
-          pickup_time: '2200',
+          picture: '',
+          pickup_time: '22:00',
           order_at: '9:00'
         },
       ],
